81f05e4ce751389cc14dcc407e4f52df696758d3
Jacopo Tagliabue <tagliabue.jacopo@gmail.com>
2024-03-28 13:10:02 -0500

    first files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Write-Audit-Publish on a data lake (no JVM!)
+
+## Overview
+
+
+## Setup
+
+
+## Running the data loader
---
+++ src/serverless/app.py
@@ -0,0 +1,12 @@
+
+
+
+def lambda_handler(event, context):
+    # print a copy of the event in cloudwatch
+    # for debugging purposes
+    print(event)
+    
+
+    return None
+
+
